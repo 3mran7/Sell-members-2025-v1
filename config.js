9167e6b645ae15276d98b04980eb3e3734060207
3mran <amranabid36@gmail.com>
2025-01-25 19:16:58 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  bot: {
+    owners: "",  // اونر
+    botID: "",    // ايدي البوت
+    GuildId: "",   // ايدي السيرفير
+    ClientId: "",    // ايدي البوت
+    serverinvte: "https://discord.gg/mayor", // انفايت سير
+    clientSECRET: "", // سكريت
+    callbackURL: "http:///login", // الكال باك
+    TheLinkVerfy : '', // رابط اوثو رايز بالصلاحيه ادخال الي سيرفرات
+    prefix : '$', 
+     TOKEN: (""),// توكن 
+    TraId  : '', // الي يتحوله كريديت
+    done : "",//ايدي روم تمت العملية
+    line : "",//رابط خط السيرفر
+    stockimg : "",//رابط صوره ستوك الاعضاء
+      supportimg : "",//رابط صوره تذكرة الدعم الفني
+    ticketimg : "",//زابط صور تذكرة الشراء
+    fedroom : "",//ايدي روم الفيدباك
+    clientrole : ""//ايدي رتبه الزباين
+  },
+  website: {
+    PORT: "6075", // بورت السيرفر
+  }
+}
